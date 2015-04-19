59ed1a908e7cbe3218eafd97a67c6958f9936d0e
Pablo Novas <pjnovas@gmail.com>
2015-04-19 20:46:02 -0300

    added menu index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+$(function(){
+
+  $('.mini-games a').jrumble({
+      x: 2,
+      y: 2,
+      rotation: 1
+    })
+    .hover(function(){
+      $(this).trigger('startRumble');
+    }, function(){
+      $(this).trigger('stopRumble');
+    });
+
+});
