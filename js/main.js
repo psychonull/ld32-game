73ae8ae5752ca976a31edfa3983f8a13d6695026
Pablo Novas <pjnovas@gmail.com>
2015-04-19 23:35:15 -0300

    added money and footsun games. updated menu view

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 
 $(function(){
 
+  var types = ['pig', 'car', 'money', 'footsun'].sort(function() {
+    return .5 - Math.random();
+  });
+
+  var ctn = $('.mini-games');
+
+  types.forEach(function(item){
+    ctn.append(
+      $('<a>')
+        .addClass(item)
+        .attr('href', "/scene-" + item + "/index.html")
+    );
+  });
+
   $('.mini-games a').jrumble({
       x: 2,
       y: 2,
